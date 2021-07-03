e6ff2943c04872a1efb28e3f75c565d66d4e60ec
DESKTOP-22JIHDU\sdddd <ffloxersuperadm@gmail.com>
2021-07-02 20:29:04 -0700

    test

--- api/src/Controller/PointsController.php
+++ api/src/Controller/PointsController.php
@@ -21,18 +21,18 @@ class PointsController extends AppController
     {
         $pointsArr = $this->Points->find()->toArray();
         //$pointsArr = mysqli_fetch_array($pointsArr);
-        //$this->set(compact("points"));
-        // $points = [
-        //     'initialPointsMale' => $pointsArr[0]['initialPoint'],
-        //     // 'sendMessageMale' => $pointsArr[0]['Points']['sendMessage'],
-        //     // 'readMessageMale' => $pointsArr[0]['Points']['readMessage'],
-        //     // 'sendImageMale' => $pointsArr[0]['Points']['sendImage'],
-        //     // 'initialPointsFemale' => $pointsArr[1]['Points']['initialPoint'],
-        //     // 'sendMessageFemale' => $pointsArr[1]['Points']['sendMessage'],
-        //     // 'readMessageFemale' => $pointsArr[1]['Points']['readMessage'],
-        //     // 'sendImageFemale' => $pointsArr[1]['Points']['sendImage'],
-        // ];
-        dd($pointsArr);
+        
+        $points = [
+            'initialPointsMale' => $pointsArr[0]['initialPoint'],
+            'sendMessageMale' => $pointsArr[0]['Points']['sendMessage'],
+            'readMessageMale' => $pointsArr[0]['Points']['readMessage'],
+            'sendImageMale' => $pointsArr[0]['Points']['sendImage'],
+            'initialPointsFemale' => $pointsArr[1]['Points']['initialPoint'],
+            'sendMessageFemale' => $pointsArr[1]['Points']['sendMessage'],
+            'readMessageFemale' => $pointsArr[1]['Points']['readMessage'],
+            'sendImageFemale' => $pointsArr[1]['Points']['sendImage'],
+        ];
+        dd($points);
         $this->set('pointsArr', compact('pointsArr'));
     }
 }
