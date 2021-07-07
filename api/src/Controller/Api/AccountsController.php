9e776bbca997f18ea81954a84445f025804500e4
DESKTOP-22JIHDU\sdddd <ffloxersuperadm@gmail.com>
2021-07-06 18:43:25 -0700

    test

--- api/src/Controller/Api/AccountsController.php
+++ api/src/Controller/Api/AccountsController.php
@@ -225,8 +225,14 @@ class AccountsController extends ApiAppController
             return $this->responseData(["error_code" => 101]);
         }
         $this->loadModel("Points");
-        $point = $this->Points->find()->select()->where(['Points.id' => $dataPost['gender']])->first();
-        
+        $point = [];
+        if ($dataPost['gender'] == 1) 
+        {
+            $point = $this->Points->find()->select()->where(['Points.male' => true])->first();
+        }
+        else{
+            $point = $this->Points->find()->select()->where(['Points.male' => false])->first();
+        }
         $account = $this->Accounts->newEntity($dataPost);
         $account->device_id = $device->id;
         $account->in_group = Account::STATUS_NORMAL;
