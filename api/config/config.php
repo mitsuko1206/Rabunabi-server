14ca8a5d9d341b5c81b43912e8e9e8321a6cd025
Senior-Dev <ffloxersuperadm@gmail.com>
2021-09-09 02:16:13 -0700

    fcm key updated

--- api/config/config.php
+++ api/config/config.php
@@ -4,8 +4,8 @@ return [
     'API' => [
         //'fcm_server_key' => 'AAAA6Q9iKbk:APA91bGNGZcQJn-MLJaHVkFni0Q0E2NMMdSJAUAmKap4cNOdNcx40Z5UXiUtGqeRYQJJ8ynhWbnfgUHuCFPvCRI9D6x91sVfK4WlLKGllHQEkNZYZ1bfs7IBSlmkZCzqYxHZbNV-8__i',
         //'fcm_server_key_android' => 'AAAA8Od2IfU:APA91bEqvi-JKugs7MKIptHWuAAbo3LdF66qoVuF9ncKXig__IKWoSQkwgLt0FRxVEAGGbUe1vhA8aX_gfQ49Clwjn6QAQXdWXaA_j8Kx0qjtZH1ic-V0Xdfv0wCNZmLHp2n3gfW7yp0',
-        'fcm_server_key' => 'AAAAOXiFeSQ:APA91bF8fdGqcvykpLQPLoctxdYnOAH0vxKj0TZyRq7BG5etZHvrZ40tLU35m6jX8a2zIb5xSPgV4Ii4xcKJSzwlhqHBaIvId2hQh2l4rRevP-KGBpAeS9s0jF4Ki9bKQONJgxVAnnRU',
-        'fcm_server_key_android' => 'AAAAOXiFeSQ:APA91bF8fdGqcvykpLQPLoctxdYnOAH0vxKj0TZyRq7BG5etZHvrZ40tLU35m6jX8a2zIb5xSPgV4Ii4xcKJSzwlhqHBaIvId2hQh2l4rRevP-KGBpAeS9s0jF4Ki9bKQONJgxVAnnRU',
+        'fcm_server_key' => 'AAAAh_8Im3U:APA91bGpx9iyRkzp0b3kUal_RbQfM3RPY1GmRRvTLh7bZCp6g3jvD4x7ccBxVx2T9w5XnMBJV34RPW2x39qqu7RHnUG3f9ZY3sC1Lg-2qAiGKjyq1CAHmc1qabxVk9vkSfMPHNTFIP_w',
+        'fcm_server_key_android' => 'AAAAh_8Im3U:APA91bGpx9iyRkzp0b3kUal_RbQfM3RPY1GmRRvTLh7bZCp6g3jvD4x7ccBxVx2T9w5XnMBJV34RPW2x39qqu7RHnUG3f9ZY3sC1Lg-2qAiGKjyq1CAHmc1qabxVk9vkSfMPHNTFIP_w',
         'salt_key' => 'BLC-PCM-2018',
         'jwt_key' => 'PCM-BLC-2018',
         'template_response' => [
